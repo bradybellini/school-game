f037ce86c0f19e7cce5da2a50d98b78799d8eb6a
bradybellini <bellinibrady65@gmail.com>
2019-11-22 23:21:57 -0700

    testing

--- Video Game/Source.cpp
+++ Video Game/Source.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+#include "Planet.h"
+#include "System.h"
+#include <string>
+
 using namespace std;
 
 int main() {
-	//tessting
-
+	Planet p;
+	Planet pp;
+	Planet ppp;
+	System s;
+	System ss;
+	cout << s.selectPlanet(2)->getPlanetName() << endl;
+	cout << p.getPlanetName() << " " << pp.getPlanetName() << " " << ppp.getPlanetName();
 	return 0;
 }
