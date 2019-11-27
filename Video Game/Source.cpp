6f7b2151d5299ada4d341f5c19ecbe9456534def
Brady <bellinibrady65@gmail.com>
2019-11-27 11:26:31 -0800

    testing

--- Video Game/Source.cpp
+++ Video Game/Source.cpp
@@ -2,13 +2,21 @@
 
 #include "Planet.h"
 #include "System.h"
+#include "Forrest.h"
+#include "Mod.h"
 #include <string>
 
 using namespace std;
 
 int main() {
 	Planet p;
-	// p.setPlanetType();
-	cout << p.getPlanetName() << " " << p.getPlanetType();
+	Forrest f;
+	System s;
+	s.addPlanets(&Planet());
+	cout << s.selectPlanet(0)->getPlanetName() << endl;
+	//cout << f.getPlanetType() << endl;
+	//cout << f.getPlanetName() << endl;
+
+
 	return 0;
 }
