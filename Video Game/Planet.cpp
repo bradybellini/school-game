50cdc28d5f4e17695bd06a6f2de6cfcc200c084c
Brady <bellinibrady65@gmail.com>
2019-11-27 11:25:58 -0800

    rethinking planet types

--- Video Game/Planet.cpp
+++ Video Game/Planet.cpp
@@ -1,19 +1,8 @@
 #include "Planet.h"
-#include <random>
-#include <time.h>
 
 Planet::Planet()
 {
-	generatePlanet();
-}
 
-void Planet::setPlanetType(std::string t)
-{
-}
-
-std::string Planet::getPlanetType()
-{
-	return planetType;
 }
 
 void Planet::setPlanetName(std::string n)
@@ -26,16 +15,12 @@ std::string Planet::getPlanetName()
 	return name.getName();
 }
 
-void Planet::generatePlanet()
+void Planet::setPlanetType(std::string t)
 {
-	int r;
+	planetType = t;
+}
 
-	std::string type[5] = { "Moon", "Arctic", "Desert", "Forest", "Magma" };
-
-	srand(time(NULL));
-
-	for (int i = 0; i < 1; i++) {
-		r = rand() % 5;
-		planetType = type[r];
-	}
+std::string Planet::getPlanetType()
+{
+	return planetType;
 }
