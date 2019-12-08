8d17195b5ba9042936b47a85cceb14d3b26a64e3
Brady <bellinibrady65@gmail.com>
2019-12-08 16:35:33 -0700

    small

--- Video Game/Weapons.h
+++ Video Game/Weapons.h
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef WEAPONS_H
+
+#include "Item.h"
+
+
+
+class Weapons : public Item {
+private:
+	int damage, price;
+	//int damageRange[2];
+	std::string attackCall, name;
+public:
+	Weapons();
+	int getDamage(), getPrice(), giveDamage();
+	std::string getName();
+	std::string getAttackCall();
+	void setPrice(int p), setDamage(int d), setName(std::string n);
+};
+
+#endif // !WEAPONS_H
